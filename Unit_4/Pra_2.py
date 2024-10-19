db31d2ba6cbaa52c576af5a8193d530f9c93bde3
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2024-10-19 11:15:42 +0530

    [Read me]

---
+++ Unit_4/Pra_2.py
@@ -0,0 +1,24 @@
+# 👉Write a programe to handle multiple execeptions like
+#   syntaxerror and typeerror.
+from multiprocessing.connection import answer_challenge
+
+try:
+    a = eval(input("Enter a dividend="))
+    b = eval(input("Enter a divisor="))
+
+    ans = a / b
+
+except (TypeError,SyntaxError):
+    print("Error")
+
+else:
+    print("Answer=",ans)
+
+#output:-
+# Enter a dividend=10
+# Enter a divisor=1
+# Answer= 10.0
+
+# Enter a dividend=10
+# Enter a divisor=k
+# Show the error
